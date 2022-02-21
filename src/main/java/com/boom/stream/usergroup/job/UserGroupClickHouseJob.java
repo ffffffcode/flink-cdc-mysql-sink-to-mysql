e2a226df7917896ad8e4b7962071e97be4a8d524
Aaron <a1233456>
2022-02-21 19:12:53 +0800

    feature: 优化Redis Sink

--- src/main/java/com/boom/stream/usergroup/job/UserGroupClickHouseJob.java
+++ src/main/java/com/boom/stream/usergroup/job/UserGroupClickHouseJob.java
@@ -1,40 +1,35 @@
 package com.boom.stream.usergroup.job;
 
-import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.core.MybatisSqlSessionFactoryBuilder;
 import com.boom.stream.usergroup.db.mapper.UserGroupMapper;
 import com.boom.stream.usergroup.enums.UserGroupDimensionEnum;
 import com.boom.stream.usergroup.enums.UserGroupOperatorEnum;
 import com.boom.stream.usergroup.param.UserGroupParam;
 import com.boom.stream.usergroup.param.UserGroupSubParam;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import com.google.common.collect.Lists;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
-import org.apache.flink.streaming.connectors.redis.RedisSink;
-import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisClusterConfig;
-import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
-import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.util.Collector;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.logging.stdout.StdOutImpl;
-import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.InetSocketAddress;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -56,26 +51,11 @@ public class UserGroupClickHouseJob {
         // TODO 解析规则
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        FlinkJedisPoolConfig flinkJedisPoolConfig = new FlinkJedisPoolConfig.Builder()
-                .setHost("10.0.10.13")
-                .setPort(23200)
-                .setDatabase(0)
-                .setPassword("a123456")
-                .build();
-
-        /*FlinkJedisClusterConfig flinkJedisClusterConfig = new FlinkJedisClusterConfig.Builder()
-                .setNodes(new HashSet<>(Collections.singletonList(new InetSocketAddress(5601)))).build();*/
 
         DataStreamSource<UserGroupParam> userGroupParamDataStreamSource = env.fromCollection(testDate()).setParallelism(1);
-        // TODO 优化去重
         userGroupParamDataStreamSource.process(new Query()).setParallelism(1)
-                .flatMap(new FlatLongMapper())
-
-                // Redis去重 单节点
-                .addSink(new RedisSink<>(flinkJedisPoolConfig, new UserGroupRedisMapper())).setParallelism(1).name("UserGroup Redis Sink");
-                // Redis去重 集群
-//                .addSink(new RedisSink<>(flinkJedisClusterConfig, new UserGroupRedisMapper())).setParallelism(1).name("UserGroup Redis Sink");
-
+                .process(new Split()).setParallelism(1)
+                .addSink(new RedisSyncSink()).setParallelism(1).name("UserGroup Redis Sink");
 //                .print().setParallelism(1).name("Print MemberId");
 
         env.execute("Print");
@@ -98,8 +78,6 @@ public class UserGroupClickHouseJob {
 
     private static class Query extends ProcessFunction<UserGroupParam, List<Long>> {
 
-//        Set<Long> result = new HashSet<>();
-
         @Override
         public void processElement(UserGroupParam userGroupParam, Context ctx, Collector<List<Long>> out) {
             try (SqlSession session = SQL_SESSION_FACTORY.openSession(true)) {
@@ -108,12 +86,10 @@ public class UserGroupClickHouseJob {
                 String time = LocalDate.now().minusDays(userGroupParam.getValue() - 1).atStartOfDay().format(DATE_TIME_FORMATTER);
                 if (userGroupParam.getIsExist()) {
                     List<Long> memberIdList = mapper.exist(userGroupParam.getTarget(), time, havingSql);
-//                    result.addAll(memberIdList);
                     out.collect(memberIdList);
                     return;
                 }
                 List<Long> memberIdList = mapper.notExist(userGroupParam.getTarget(), time, havingSql);
-//                result.addAll(memberIdList);
                 out.collect(memberIdList);
             }
         }
@@ -164,6 +140,75 @@ public class UserGroupClickHouseJob {
         }
     }
 
+    private static class Split extends ProcessFunction<List<Long>, List<Long>> {
+        @Override
+        public void processElement(List<Long> value, Context ctx, Collector<List<Long>> out) throws Exception {
+            for (List<Long> list : Lists.partition(value, 10000)) {
+                out.collect(new ArrayList<>(list));
+            }
+        }
+    }
+
+
+    private static class RedisSyncSink extends RichSinkFunction<List<Long>> {
+
+        private RedisCommands<String, String> redisSyncCommands;
+        private StatefulRedisConnection<String, String> connect;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            RedisURI redisUri = RedisURI.Builder.redis("10.0.10.13", 23200).withPassword("a123456").withDatabase(0).build();
+            RedisClient redisClient = RedisClient.create(redisUri);
+            connect = redisClient.connect();
+            redisSyncCommands = connect.sync();
+        }
+
+        @Override
+        public void invoke(List<Long> value, Context context) throws Exception {
+            redisSyncCommands.sadd(redisKey, value.stream().map(String::valueOf).toArray(String[]::new));
+        }
+
+        @Override
+        public void close() throws Exception {
+            if (redisSyncCommands != null) {
+                redisSyncCommands.shutdown(true);
+            }
+            if (connect != null) {
+                connect.close();
+            }
+        }
+    }
+
+
+    /*private static class RedisAsyncSink extends RichSinkFunction<List<Long>> {
+
+        private RedisAsyncCommands<String, String> redisAsyncCommands;
+        private StatefulRedisConnection<String, String> connect;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            RedisURI redisUri = RedisURI.Builder.redis("10.0.10.13", 23200).withPassword("a123456").withDatabase(0).build();
+            RedisClient redisClient = RedisClient.create(redisUri);
+            connect = redisClient.connect();
+            redisAsyncCommands = connect.async();
+        }
+
+        @Override
+        public void invoke(List<Long> value, Context context) throws Exception {
+            redisAsyncCommands.sadd(redisKey, value.stream().map(String::valueOf).toArray(String[]::new));
+        }
+
+        @Override
+        public void close() throws Exception {
+            if (redisAsyncCommands != null) {
+                redisAsyncCommands.shutdown(true);
+            }
+            if (connect != null) {
+                connect.close();
+            }
+        }
+    }*/
+
     public static SqlSessionFactory initSqlSessionFactory() {
         Reader reader = null;
         try {
@@ -174,30 +219,4 @@ public class UserGroupClickHouseJob {
         return new MybatisSqlSessionFactoryBuilder().build(reader, "dev");
     }
 
-    private static class UserGroupRedisMapper implements RedisMapper<String> {
-        @Override
-        public RedisCommandDescription getCommandDescription() {
-            return new RedisCommandDescription(RedisCommand.SADD);
-        }
-
-        @Override
-        public String getKeyFromData(String value) {
-            return redisKey;
-        }
-
-        @Override
-        public String getValueFromData(String value) {
-            return value;
-        }
-
-    }
-
-    private static class FlatLongMapper implements FlatMapFunction<List<Long>, String> {
-        @Override
-        public void flatMap(List<Long> value, Collector<String> out) throws Exception {
-            for (Long l : value) {
-                out.collect(String.valueOf(l));
-            }
-        }
-    }
 }
