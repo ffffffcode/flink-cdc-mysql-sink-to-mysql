cb515100c306c7bc4a571b990269b8fcc0d48039
Aaron <a1233456>
2022-02-23 15:28:39 +0800

    feature: 合并流方式Sink

--- src/main/java/com/boom/stream/userbehavior/job/MySqlBinlogSourceUserBehaviorClickHouseSinkJob.java
+++ src/main/java/com/boom/stream/userbehavior/job/MySqlBinlogSourceUserBehaviorClickHouseSinkJob.java
@@ -1,14 +1,19 @@
 package com.boom.stream.userbehavior.job;
 
 import com.boom.stream.userbehavior.deserializer.UserBehaviorDebeziumDeserializer;
+import com.boom.stream.userbehavior.deserializer.UserReviewBehaviorDebeziumDeserializer;
 import com.boom.stream.userbehavior.entity.UserBehavior;
+import com.ververica.cdc.connectors.mongodb.MongoDBSource;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
 import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
 import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.CoMapFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -37,13 +42,28 @@ public class MySqlBinlogSourceUserBehaviorClickHouseSinkJob {
                     .debeziumProperties(debeziumProperties)
                     .deserializer(new UserBehaviorDebeziumDeserializer())
                     .build();
+            SourceFunction<UserBehavior> mongoSource = MongoDBSource.<UserBehavior>builder()
+                    .hosts("10.0.10.13:23301,10.0.10.13:23302,10.0.10.13:23303")
+                    .username("bm")
+                    .password("a123456")
+                    .database("bm-mall")
+                    .collection("review")
+                    .connectionOptions("authSource=bm-mall")
+                    .deserializer(new UserReviewBehaviorDebeziumDeserializer())
+                    .build();
 
             StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
             env.enableCheckpointing(3000L);
 
+            DataStreamSource<UserBehavior> reviewMongodbOplogSource = env.addSource(mongoSource, "review MongoDB Oplog Source")
+                    .setParallelism(1);
+
+
+
             env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "order/order_check_code/order_refund/my_collect/overlord_meal MySQL Binlog Source")
                     .setParallelism(1)
+                    .connect(reviewMongodbOplogSource).map(new NothingToDoCoMapper())
                     .addSink(JdbcSink.sink(
                             "INSERT INTO statistics_user_behavior (tenant_id, area_id, member_id, event_time, behavior_type, behavior_name, source_id, actual_pay_money) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
                             (ps, t) -> {
@@ -80,4 +100,15 @@ public class MySqlBinlogSourceUserBehaviorClickHouseSinkJob {
         }
     }
 
+    private static class NothingToDoCoMapper implements CoMapFunction<UserBehavior, UserBehavior, UserBehavior> {
+        @Override
+        public UserBehavior map1(UserBehavior value) throws Exception {
+            return value;
+        }
+
+        @Override
+        public UserBehavior map2(UserBehavior value) throws Exception {
+            return value;
+        }
+    }
 }
